0a8f79c62795a34791baea9d3325676872cbcb4f
Edward-Kabue <ekabue0@gmail.com>
2024-02-20 18:45:43 +0300

    Add ContactController api and add the index method

--- resources/views/contact/create.blade.php
+++ resources/views/contact/create.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.base')
 @section('content')
     <form action="{{ route('contact.store') }}" method="POST" enctype="multipart/form-data">
         {{-- csrf is a middleware that protects your application
